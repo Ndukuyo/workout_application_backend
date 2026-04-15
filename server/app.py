c648fd7da5026d2f3044e948b0cccd0ee631e753
Ndukuyo <harrisonndukuyo@gmail.com>
2026-04-15 12:52:31 +0300

    scaffold of all endpoints

--- server/app.py
+++ server/app.py
@@ -1,17 +1,62 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
-
-from models import *
+from models import db, Exercise, Workout, WorkoutExercise
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-# Define Routes here
+
+
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+
+    pass
+
+@app.route('/workouts/<int:id>', methods=['GET'])
+def get_workout(id):
+
+    pass
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+
+    pass
+
+@app.route('/workouts/<int:id>', methods=['DELETE'])
+def delete_workout(id):
+  
+    pass
+
+
+
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+
+    pass
+
+@app.route('/exercises/<int:id>', methods=['GET'])
+def get_exercise(id):
+
+    pass
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+
+    pass
+
+@app.route('/exercises/<int:id>', methods=['DELETE'])
+def delete_exercise(id):
+
+    pass
+
+@app.route('/workouts/<int:workout_id>/exercises/<int:exercise_id>/workout_exercises', methods=['POST'])
+def add_exercise_to_workout(workout_id, exercise_id):
+
+    pass
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
